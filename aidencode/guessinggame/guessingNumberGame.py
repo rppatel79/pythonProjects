2c13c768a55af351431daa657a8ca15dc9fd6715
Ritesh Patel <ritesh.pri.patel@gmail.com>
2020-08-15 14:07:51 -0700

    keep track of each players score

--- aidencode/guessinggame/guessingNumberGame.py
+++ aidencode/guessinggame/guessingNumberGame.py
@@ -3,18 +3,17 @@ import random
 TOTAL_NUMBER_OF_GAMES = 2
 PLAYERS = 2
 
+player1Tries = 0
+player2Tries = 0
 
+for playerId in range(1, PLAYERS + 1):
 
-for playerId in range(1, PLAYERS+1):
-    currentPlayerTries = 0
-    print("Hello Player ["+str(playerId))+"]"
-
-    for gameId in range(1, TOTAL_NUMBER_OF_GAMES+1):
+    for gameId in range(1, TOTAL_NUMBER_OF_GAMES + 1):
         humanTriesInAGame = 1
 
         # the computer makes a guess
         computerGuess = int(random.random() * 10)
-        #print("Computer guessed:  " + str(computerGuess))
+        # print("Computer guessed:  " + str(computerGuess))
 
         # reset humanGuess
         humanGuess = -1
@@ -22,7 +21,8 @@ for playerId in range(1, PLAYERS+1):
         # loop
         while humanGuess != computerGuess:
             # player makes a guess
-            humanGuess = int(input("What is your guess (try[" + str(humanTriesInAGame) + "])? :"))
+            humanGuess = int(input("Player["+str(playerId)+"]-Game["+str(gameId)+"] What is your guess (try[" +
+                                   str(humanTriesInAGame) + "])? :"))
 
             if humanGuess == computerGuess:
                 print("You guessed right!!")
@@ -30,6 +30,14 @@ for playerId in range(1, PLAYERS+1):
                 print("You loser, try again.")
                 humanTriesInAGame += 1
 
-        print("Game " + str(gameId) + " over")
-        currentPlayerTries += humanTriesInAGame
-    print("Player[+"+str(playerId)+"] -Your games are over- Your total score was [" + str(currentPlayerTries) + "]")
+        print("Player["+str(playerId)+"]-Game [" + str(gameId) + "] over")
+
+        if playerId == 1:  # Player 1's turn
+            player1Tries += humanTriesInAGame
+        else:
+            player2Tries += humanTriesInAGame
+
+    if playerId == 1:  # Player 1's turn
+        print("Player[" + str(playerId) + "] -Your games are over- Your total score was [" + str(player1Tries) + "]")
+    else:
+        print("Player[" + str(playerId) + "] -Your games are over- Your total score was [" + str(player2Tries) + "]")
