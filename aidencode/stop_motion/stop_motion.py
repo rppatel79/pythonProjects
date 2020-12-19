3260d497848e3cb8fd0f9f1874d4c95c0d121132
Ritesh Patel <ritesh.pri.patel@gmail.com>
2020-12-19 09:57:26 -0700

    added loop capabilities

--- aidencode/stop_motion/stop_motion.py
+++ aidencode/stop_motion/stop_motion.py
@@ -3,25 +3,16 @@ from time import sleep
 
 camera = PiCamera()
 
-camera.rotation=180
+camera.rotation = 180
 
 camera.start_preview()
-sleep(3)
-camera.capture('/home/pi/Desktop/image.jpg')
-camera.stop_preview()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+frame = 1
+while True:
+    try:
+        input('Press Enter to take a picture')
+        sleep(1)
+        camera.capture('/home/pi/stop_motion/images/frame%03d.jpg' % frame)
+        frame += 1
+    except KeyboardInterrupt:
+        camera.stop_preview()
+        break
