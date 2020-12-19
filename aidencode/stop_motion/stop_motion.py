54aba993f091746d611504639b5cd4e3552b1fc1
Ritesh Patel <ritesh.pri.patel@gmail.com>
2020-12-18 17:05:53 -0700

    started to create stop motion video

---
+++ aidencode/stop_motion/stop_motion.py
@@ -0,0 +1,24 @@
+from picamera import PiCamera
+from time import sleep
+
+camera = PiCamera()
+camera.start_preview()
+sleep(3)
+camera.capture('/home/pi/Desktop/image.jpg')
+camera.stop_preview()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
