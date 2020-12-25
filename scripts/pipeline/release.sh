a45dde889e455b65c3dcb14dd657c0c43cc6aa81
Ritesh Patel <ritesh.pri.patel@gmail.com>
2020-12-25 12:07:40 -0700

    added release script

---
+++ scripts/pipeline/release.sh
@@ -0,0 +1,11 @@
+bucket_name=$1
+local_resource=$2
+aws_access_key=$3
+aws_access_secret=$4
+local_path=$5
+
+# Install required dependencies for Python script.
+pip3 install boto3
+
+# Run upload script
+python3 scripts/pipeline/upload_file_to_s3.py $bucket_name $local_resource $aws_access_key $aws_access_secret $local_path
