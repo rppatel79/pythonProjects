3de91b0ee6cd8c18f4e0561224a6b26ffb9cfde0
Ritesh Patel <ritesh.pri.patel@gmail.com>
2020-12-31 10:39:16 -0700

    code for the cat feeder project

---
+++ aidencode/cat_feeder/cat_feeder.py
@@ -0,0 +1,29 @@
+# Imports
+import RPi.GPIO as GPIO
+import time
+
+# set GPIO numbering mode
+GPIO.setmode(GPIO.BOARD)
+
+# set pin 11 as an output, and set servo1 as pin 11 as PWM
+GPIO.setup(11, GPIO.OUT)
+servo1 = GPIO.PWM(11, 50) # note 11 is pin, 50= 50 Hz pulse
+
+try:
+        #start PWM running, but with value of 0 (publse off)
+        servo1.start(0)
+        print("Waiting for 2 seconds")
+        time.sleep(2)
+
+        # rotate the servo 90 deg and than back
+        servo1.ChangeDutyCycle(7.5) # neutral position
+        time.sleep(1)
+        servo1.ChangeDutyCycle(12.5) # right +90 deg position
+        time.sleep(1)
+        servo1.ChangeDutyCycle(7.5) # neutral position
+        time.sleep(1)
+finally:
+        servo1.stop()
+        GPIO.cleanup()
+
+print("Goodbye")
