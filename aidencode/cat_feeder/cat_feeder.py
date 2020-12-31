c837961e6ac2f0800a6b6bfbbbdcd8267d1ba100
ritesh.pri.patel@gmail.com <ritesh.pri.patel@gmail.com>
2020-12-31 18:10:11 +0000

    added waiting time parameter

--- aidencode/cat_feeder/cat_feeder.py
+++ aidencode/cat_feeder/cat_feeder.py
@@ -1,6 +1,13 @@
 # Imports
 import RPi.GPIO as GPIO
 import time
+import sys
+
+wait_time = 1
+if len(sys.argv) == 2:
+	wait_time = float(sys.argv[1])
+
+print("Wait Time is:",wait_time)
 
 # set GPIO numbering mode
 GPIO.setmode(GPIO.BOARD)
@@ -12,14 +19,13 @@ servo1 = GPIO.PWM(11, 50) # note 11 is pin, 50= 50 Hz pulse
 try:
         #start PWM running, but with value of 0 (publse off)
         servo1.start(0)
-        print("Waiting for 2 seconds")
         time.sleep(2)
 
         # rotate the servo 90 deg and than back
         servo1.ChangeDutyCycle(7.5) # neutral position
         time.sleep(1)
         servo1.ChangeDutyCycle(12.5) # right +90 deg position
-        time.sleep(1)
+        time.sleep(wait_time)
         servo1.ChangeDutyCycle(7.5) # neutral position
         time.sleep(1)
 finally:
