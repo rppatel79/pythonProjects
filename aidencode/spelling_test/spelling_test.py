cbacdaa2f40a13dc1000d62f8bafb69492efb94a
Ritesh Patel <ritesh.pri.patel@gmail.com>
2021-03-23 16:02:38 -0700

    Added __main__

--- aidencode/spelling_test/spelling_test.py
+++ aidencode/spelling_test/spelling_test.py
@@ -3,26 +3,28 @@ import pandas as pd
 import random
 import json
 
-with open("data/words.json") as json_file:
-    weeklyWords = json.load(json_file)
 
-playersName = "Aiden"
-engine = pyttsx3.init()
-engine.say("Hello " + playersName + "!")
-engine.runAndWait()
+if __name__ == "__main__":
+    with open("data/words.json") as json_file:
+        weeklyWords = json.load(json_file)
 
-score = 0
-for randomWord in random.sample(weeklyWords["2"], len(weeklyWords["2"])):
+    playersName = "Aiden"
     engine = pyttsx3.init()
-    engine.say("Please spell the word:  " + randomWord)
-    engine.runAndWait()
-    userGuess = input("Enter your guess: ")
-    if userGuess == randomWord:
-        engine.say("Great job " + playersName + "!")
-        score += 1
-    else:
-        engine.say("Sorry, that is incorrect.")
+    engine.say("Hello " + playersName + "!")
     engine.runAndWait()
 
-engine.say("Your score was " + str(score) + " out of "+str(len(weeklyWords["2"])))
-engine.runAndWait()
+    score = 0
+    for randomWord in random.sample(weeklyWords["2"], len(weeklyWords["2"])):
+        engine = pyttsx3.init()
+        engine.say("Please spell the word:  " + randomWord)
+        engine.runAndWait()
+        userGuess = input("Enter your guess: ")
+        if userGuess == randomWord:
+            engine.say("Great job " + playersName + "!")
+            score += 1
+        else:
+            engine.say("Sorry, that is incorrect.")
+        engine.runAndWait()
+
+    engine.say("Your score was " + str(score) + " out of "+str(len(weeklyWords["2"])))
+    engine.runAndWait()
