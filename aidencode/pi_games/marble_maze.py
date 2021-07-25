e6c1302a62ae63a0a4a2fb31f38d9411f6eb54e2
ritesh.pri.patel@gmail.com <ritesh.pri.patel@gmail.com>
2021-07-25 19:41:41 +0100

    cleaned up main

--- aidencode/pi_games/marble_maze.py
+++ aidencode/pi_games/marble_maze.py
@@ -55,33 +55,37 @@ maze1 = [[r,r,r,r,r,r,r,r],
 #         [r,r,r,r,r,r,r,b],
 #         [b,b,b,b,b,b,b,b]]
 
-game_over = False
+def start():
+	game_over = False
 
-ball_x_position =1
-ball_y_position =1
+	ball_x_position =1
+	ball_y_position =1
 
-hole_x_position =0
-hole_y_position =7
+	hole_x_position =0
+	hole_y_position =7
 
-while not game_over:
-	o = sense.get_orientation()
-	pitch = o["pitch"]
-	roll = o["roll"]
-	yaw = o["yaw"]
+	while not game_over:
+		o = sense.get_orientation()
+		pitch = o["pitch"]
+		roll = o["roll"]
+		yaw = o["yaw"]
 
-	new_ball_x_position,new_ball_y_position = move_marble(pitch,roll,ball_x_position,ball_y_position)
+		new_ball_x_position,new_ball_y_position = move_marble(pitch,roll,ball_x_position,ball_y_position)
 
-	maze1[hole_y_position][hole_x_position]=hole_color
-	maze1[ball_y_position][ball_x_position]=b
-	sense.set_pixels(sum(maze1,[]))
+		maze1[hole_y_position][hole_x_position]=hole_color
+		maze1[ball_y_position][ball_x_position]=b
+		sense.set_pixels(sum(maze1,[]))
 
-	maze1[new_ball_y_position][new_ball_x_position]=ball_color
-	sense.set_pixels(sum(maze1,[]))
+		maze1[new_ball_y_position][new_ball_x_position]=ball_color
+		sense.set_pixels(sum(maze1,[]))
 
-	time.sleep(0.05)
-	ball_y_position = new_ball_y_position
-	ball_x_position = new_ball_x_position
+		time.sleep(0.05)
+		ball_y_position = new_ball_y_position
+		ball_x_position = new_ball_x_position
 
-	if ball_x_position == hole_x_position and ball_y_position == hole_y_position:
-		sense.show_message("you WIN!")
-		game_over = True
+		if ball_x_position == hole_x_position and ball_y_position == hole_y_position:
+			sense.show_message("you WIN!")
+			game_over = True
+
+if __name__ ==	"__main__":
+	start()
