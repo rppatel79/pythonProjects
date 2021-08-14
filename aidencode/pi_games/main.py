95b6cde63a5796d04aac966fe7e037028afa74d2
ritesh.pri.patel@gmail.com <ritesh.pri.patel@gmail.com>
2021-08-14 23:36:33 +0100

    updating main game

--- aidencode/pi_games/main.py
+++ aidencode/pi_games/main.py
@@ -1,8 +1,32 @@
 from sense_hat import SenseHat
 import time
+import basher
+import snake_games
+import marble_maze
 
+sense = SenseHat()
 def main():
-    print("Hello World!")
+	while True:
+		sense.show_message("Up basher, Left snake, Down-marble maze")
+
+		event = sense.stick.wait_for_event(emptybuffer=True)
+		direction = event.direction
+		print ("got event",flush=True)
+		if direction == "right":
+                	#RANDOM
+			print ("not done yet",flush=True)
+		elif direction  == "left":
+			print ("starting snake",flush=True)
+			snake_games.start()
+		elif direction == "up":
+			print ("starting basher",flush=True)
+			basher.start()
+		elif direction == "down":
+			print ("starting marble_maze",flush=True)
+			marble_maze.start()
+		else:
+			print("Unknown command")
+		sense.clear()
 
 if __name__ == "__main__":
-    main()
+	main()
