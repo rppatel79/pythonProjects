2dbba3ec323d0e37566ccacc9927abb1d4550e92
Ritesh Patel <ritesh.pri.patel@gmail.com>
2022-04-30 16:46:15 -0700

    generalized code

--- aidencode/stop_motion_plant/create_video.sh
+++ aidencode/stop_motion_plant/create_video.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 
 TMP_LOC=/home/pi/tmp/
+DEFAULT_FILENAME=plant_video_420
+DEFAULT_BUCKETNAME=plant.photo.bucket
 
-file_name=animation
+if [ "$#" = "0" ]
+then
+	file_name=$DEFAULT_FILENAME
+	bucket_name=$DEFAULT_BUCKETNAME
+elif [ "$#" = "2" ]
+then
+	file_name=$1
+	bucket_name=$2
+else
+	echo "Illegal number of parameters"
+	exit -1
+fi
+
 current_time=$(date "+%Y.%m.%d-%H.%M.%S")
-new_fileName=$file_name.$current_time.mp4
+echo "Starting video at $current_time"
+
+#new_fileName=$file_name.$current_time.mp4
+new_fileName=$file_name.mp4
 echo "New FileName: " "$new_fileName"
+echo "Bucket name: " "$bucket_name"
 
 #AWS CLI
 #sudo pip install awscli
@@ -14,10 +32,10 @@ mkdir -p $TMP_LOC
 cd $TMP_LOC
 
 echo "Downloading files"
-aws s3 sync s3://plant.photo.bucket $TMP_LOC
+/usr/local/bin/aws s3 sync s3://$bucket_name $TMP_LOC
 
 echo "Creating video in $TMP_LOC $new_fileName"
-ffmpeg -framerate 25 -pattern_type glob -i '*.jpg' -vf "transpose=3"  -c:v libx264 -pix_fmt yuv420p $new_fileName
+ffmpeg -framerate 12 -pattern_type glob -i '*.jpg' -vf "transpose=2"  -c:v libx264 -pix_fmt yuv420p $new_fileName
 
 retVal=$?
 if [ $retVal -ne 0 ]; then
